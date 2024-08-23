02ca83212d58da88ae4d9ad03266068e556779ac
mickslow <go.acosta3@gmail.com>
2024-08-22 22:53:31 -0300

    pokeapi

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Pedido from "./components/Pedido";
+import PokeApi from "./components/PokeApi";
 
 function App() {
   return (
     <>
       <Form></Form>
+      <h3>
+        ---------------------------------------- Semana 5
+        ----------------------------------------
+      </h3>
       <Pedido />
+      <PokeApi></PokeApi>
     </>
   );
 }
---
+++ src/components/PokeApi.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function PokeApi() {
+  const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState();
+  const url = "https://pokeapi.co/api/v2/pokemon/cubone";
+
+  useEffect(() => {
+    axios(url).then((res) => {
+      setTimeout(() => {
+        setLoading(false);
+        setPokemon(res.data);
+      }, 850);
+    });
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h5>Cargando pokemon...</h5>
+      ) : (
+        <>
+          <h1 className="pokeName">{pokemon.name}</h1>
+          <img src={pokemon.sprites.front_default} alt="" width={200} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default PokeApi;
